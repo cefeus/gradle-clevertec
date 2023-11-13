9a820b0e35daa87a4719f49f3f4e00677df037f6
cefe!s <fireinspirer@gmail.com>
2023-11-13 13:13:40 +0300

    fix: fixed test and build.gradle files

--- utiljar/src/test/java/IsPositiveTest.java
+++ utiljar/src/test/java/IsPositiveTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Test;
 import util.StringUtils;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -5,10 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IsPositiveTest {
 
-    @org.junit.jupiter.api.Test
-    void stringUtilsTest() {
-        String[] numbers = {"0", "-1", "2", "3", "4"};
+    @Test
+    void stringUtilsTest_allPositiveNumbers_shouldBeTrue() {
+        String[] numbers = {"0", "1", "2", "3", "4"};
         assertTrue(StringUtils.isPositiveNumber(numbers[0]));
+    }
+
+    @Test
+    void stringUtilsTest_fewNegativeNumbers_shouldBeFalse() {
+        String[] numbers = {"0", "-1", "2", "-3", "4"};
         assertFalse(StringUtils.isPositiveNumber(numbers[1]));
     }
 }
